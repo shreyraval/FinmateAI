0485c5919e8261a2692be75a830f4bb1607b4dfc
Shrey Raval <shreyraval710@gmail.com>
2025-05-25 00:48:20 -0700

    Add streamlit frontend

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests
 
-st.title("FinMate – Upload Bank Statement")
+st.title("FinmateAI – Upload Bank Statement")
 uploaded = st.file_uploader("Choose PDF or CSV")
 if uploaded:
-    response = requests.post("http://localhost:8000/upload/", files={"file": uploaded})
+    response = requests.post("https://finmateai.onrender.com/upload/", files={"file": uploaded})
     st.json(response.json())
